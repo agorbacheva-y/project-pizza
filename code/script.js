48f76fdced15589dee601951e70193cf851646b7
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-28 16:02:45 +0200

    step 3

--- code/script.js
+++ code/script.js
@@ -22,13 +22,13 @@ const foodMenu = prompt(
 let selectedFood = "";
 switch(foodMenu) {
   case "1":
-    selectedFood = "pizza";
+    selectedFood = "Pizza";
     break;
   case "2":
-    selectedFood = "pasta";
+    selectedFood = "Pasta";
     break;
   case "3":
-    selectedFood = "salad";
+    selectedFood = "Salad";
     break;
   default:
     alert('Invalid choice. Please choose a number between 1 and 3.');
@@ -39,6 +39,43 @@ alert(`You've chosen ${selectedFood}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
+let subMenu = "";
+
+switch(selectedFood) {
+  case "Pizza":
+    subMenu = prompt(
+      `Select a ${selectedFood} type:
+        Enter a number:
+        1 - Margherita
+        2 - Pepperoni
+        3 - Vegetable`
+    );
+    break;
+
+  case "Pasta":
+    subMenu = prompt(
+      `Select a ${selectedFood} type:
+        Enter a number:
+        1 - Bolognese
+        2 - Seafood
+        3 - Carbonara`
+    );
+    break;
+
+  case "Salad":
+    subMenu = prompt(
+      `Select a ${selectedFood} type:
+        Enter a number:
+        1 - Caesar
+        2 - Greek
+        3 - Cobb`
+    );
+    break;
+      
+  default:
+      alert('Invalid choice. Please choose a number between 1 and 3.');
+      exit(1);
+}
 
 // Step 4 - Age
 // Your code goes here

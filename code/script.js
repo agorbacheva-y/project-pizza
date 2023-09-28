897fd4dafcc0e3ad7406cf6c4843716af00133b4
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-28 15:55:03 +0200

    step 2

--- code/script.js
+++ code/script.js
@@ -12,6 +12,30 @@ alert(`Hi ${username}!`);
 
 // Step 2 - Food choice
 // Your code goes here
+const foodMenu = prompt(
+  `What type of food would you like to order?
+  1 - Pizza
+  2 - Pasta
+  3 - Salad`
+);
+
+let selectedFood = "";
+switch(foodMenu) {
+  case "1":
+    selectedFood = "pizza";
+    break;
+  case "2":
+    selectedFood = "pasta";
+    break;
+  case "3":
+    selectedFood = "salad";
+    break;
+  default:
+    alert('Invalid choice. Please choose a number between 1 and 3.');
+    exit(1);
+};
+
+alert(`You've chosen ${selectedFood}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
